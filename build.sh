84e69d9674ed1281216affd2a4f07a5e2a699e52
Sajid Moinuddin <sajid.moinuddin@foxsports.com.au>
2019-03-08 16:55:41 +1100

    update run command

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker build -t sajid2045/gitops-toolkit .
 
 # docker build -f Dockerfile-ccloud -t sajid2045/ccloud .
-# docker run -it --rm -v ~/:/root/ sajid2045/ccloud /bin/bash
+# docker run -it --rm -v ~/.ccloud:/root/.ccloud  sajid2045/ccloud
 
