9836ca434815b81ccfe679afce1b89cfebcc7ba1
Sajid Moinuddin <sajid.moinuddin@foxsports.com.au>
2019-05-28 12:12:27 +1000

    jx version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build -t sajid2045/gitops-toolkit .
+docker build -t sajid2045/gitops-toolkit:jx-1.3.783 .
 
 # docker build -f Dockerfile-ccloud -t sajid2045/ccloud .
 # docker run -it --rm -v ~/.ccloud:/root/.ccloud  sajid2045/ccloud
